3e35896c0b3a0da2664cd29dbd99aad71e2a450d
kimseongeun <sekim520@gmail.com>
2023-12-11 15:15:01 +0900

    fix : client2 - servo control - pixel to distance - lid detection

--- servo/client2.c
+++ servo/client2.c
@@ -1,6 +1,5 @@
 #include "./module.h"
 
-// #define SERVO1 1 // -> GPIO18, PWM0
 #define SERVO1 0 // PWM
 #define SERVO2 23 // wPi pin number for servo
 #define SERVO3 24 
@@ -11,8 +10,8 @@
 #define PWM_PERIOD 20000000
 #define SERVO_MAX_DUTY 13
 #define SERVO_MIN_DUTY 2.5
-#define LID_MIN 5
-#define LID_MAX 10
+#define LID_MIN 3
+#define LID_MAX 7
 #define SOCK 3
 
 int connect_socket(int argc, char *argv[]) {
@@ -93,7 +92,7 @@ void* ultrasonic() {
     double time, distance;
     int count = 0; // 3초 이상 뚜껑이 감지됐을 경우 서버에 알리기 위한 count
     printf("* Detecting lid... %d < distance < %d\n", LID_MIN, LID_MAX);
-
+    usleep(1000000);
     while (1) {
         if (-1 == GPIOWrite(POUT, 1)) {
             printf("gpio write/trigger err\n");
@@ -114,10 +113,11 @@ void* ultrasonic() {
         // printf("time : %.4lf\n", time);
         printf("\tdistance : %.2lfcm\n", distance);
 
-        if (LID_MIN < distance && distance < LID_MAX) {
+        if (distance < LID_MIN || LID_MAX < distance) { // 뚜껑이 원래 위치에서 벗어남 -> 즉 뚜껑을 웨이퍼에 덮음
             count++;
             if (count > 6) {
-                write(SOCK, "LID", sizeof("LID")); // 뚜껑이 감지된 것을 서버에 알림.  
+                usleep(5000000);
+                write(SOCK, "LID", sizeof("LID")); // 뚜껑이 덮힌 것을 서버에 알림.  
                 printf("* Lid is on\n");
                 return 0;
             }
@@ -133,25 +133,32 @@ void* ultrasonic() {
 int move_wafer(char dir, int dist) {
     int duty;
     duty = pixel2duty(dist);
+    min_duty = PWM_PERIOD * SERVO_MIN_DUTY / 100;
+    // dist 만큼 모터가 움직이고 다시 원래 자리로 위치
     if (dir == 'U') { 
-        printf("* Move down \n");
-        duty = duty / 100000;
-        softPwmWrite(SERVO1, duty);
+        printf("* Move down \n\n");
+        softPwmWrite(SERVO4, duty/100000);
+        usleep(1000000);
+        softPwmWrite(SERVO4, min_duty/100000);
     }
     else if (dir == 'D') {
-        printf("* Move up \n");
-        duty = duty / 100000;
-        softPwmWrite(SERVO2, duty);
-    }
-    else if (dir == 'R') {
-        printf("* Move left \n");
-        duty = duty / 100000;
-        softPwmWrite(SERVO3, duty);
+        printf("* Move up \n\n");
+        softPwmWrite(SERVO3, duty/100000);
+        usleep(1000000);
+        softPwmWrite(SERVO3, min_duty/100000);
+
     }
     else if (dir == 'L') {
-        printf("* Move right \n");
-        // softPwmWrite(SERVO4, duty);
-        PWMWriteDutyCycle(SERVO4, duty);
+        printf("* Move right \n\n");
+        softPwmWrite(SERVO2, duty/100000);
+        usleep(1000000);
+        softPwmWrite(SERVO2, min_duty/100000);
+    }
+    else if (dir == 'R') {
+        printf("* Move left \n\n");
+        PWMWriteDutyCycle(SERVO1, duty);
+        usleep(1000000);
+        PWMWriteDutyCycle(SERVO1, min_duty);
     }
     usleep(1000000);
     return 0;
@@ -181,8 +188,6 @@ void* control_servo(char msg[]) {
     move_wafer(d1, n1);
     move_wafer(d2, n2);
 
-    init_values();
-
     ultrasonic();
     printf("* Exit servo thread\n");
     pthread_exit(0);
@@ -191,11 +196,11 @@ void* control_servo(char msg[]) {
 int pixel2duty(int pixel) {
     int dist, duty, percent;
     double deg;
-    // pixel to distance
-    dist = pixel*(4);
+    // pixel to distance 121 <-> 10mm
+    dist = pixel * 10 / 121;
 
     // distance to degree : 3mm <-> 10도
-    deg = (10/3) * dist;
+    deg = (10 / 3) * dist;
 
     // degree to duty
     percent = SERVO_MIN_DUTY + (deg * (SERVO_MAX_DUTY - SERVO_MIN_DUTY) / 180.0);
@@ -204,42 +209,11 @@ int pixel2duty(int pixel) {
     return duty;
 }
 
-void* read_server() {
-	char msg[100];
-	int str_len;
-    int flag = 0; // check if servo control thread has created
-    pthread_t p_thread[1];
-    int thr_id, status;
-    printf("* Read from server\n");
-
-	while(1) {
-        str_len = read(SOCK, msg, sizeof(msg));
-    
-		if (str_len == -1) {
-            error_handling("read() error");
-            
-        }
-        else if (str_len > 0) {
-            printf("* msg from server : %.*s\n", str_len, msg);
-            if (strncmp(msg, "Stop", 4) * strncmp(msg, "Error", 5) == 0) {
-                // 종료 신호
-                if (flag == 1){
-                    pthread_cancel(p_thread[0]);
-                }
-                pthread_exit(0);
-            }
-            else if(strncmp(msg, "Camera", 6)){
-                // Camera
-                thr_id = pthread_create(&p_thread[0], NULL, control_servo, msg);
-                flag = 1;
-            }
-        }
-	}
-}
-
 void (*breakCapture)(int);
 
 void exit_process(int signo) {
+    // close socket connection
+    close(SOCK);
     // unexport pins
     GPIOUnexport(PIN);
     GPIOUnexport(POUT);
@@ -265,17 +239,39 @@ int main(int argc, char *argv[]) {
 
 	// create threads
 	pthread_t p_thread[1];
-	int thr_id;
-	int status;
+	int thr_id, status;
 
     // read server
-    thr_id = pthread_create(&p_thread[0], NULL, read_server, NULL);
-	if (thr_id < 0) {
-		perror("-> thread create error : ");
-		exit(1);
+    char msg[256];
+	int str_len;
+    int flag = 0; // check if servo control thread has created
+    printf("* Read from server\n");
+
+    while(1) {
+        str_len = read(SOCK, msg, sizeof(msg));
+    
+		if (str_len == -1) {
+            error_handling("read() error");
+            
+        }
+        else if (str_len > 0) {
+            printf("* msg from server : %s\n", msg);
+            if (strncmp(msg, "Stop", 4) * strncmp(msg, "Error", 5) * strncmp(msg, "End", 3) == 0) {
+                // 종료 신호
+                if (flag == 1){
+                    pthread_cancel(p_thread[0]);
+                }
+                break;
+            }
+            else {
+                // Camera
+                thr_id = pthread_create(&p_thread[0], NULL, control_servo, msg);
+                flag = 1;
+            }
+        }
+        usleep(500000);
 	}
     
-    pthread_join(p_thread[0], (void **)&status);
     exit_process(0);
     
     return 0;
